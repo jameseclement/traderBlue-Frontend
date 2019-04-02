fdda890cdf6445d91dbbe025d9c594939bd36e6a
James Clement <jameseclement@gmail.com>
2019-04-02 15:58:18 -0400

    unstyled navigation to three main pages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
-    return <div>Navbarrrrrrrrrrr</div>;
+    return (
+      <div className="ui inverted teal menu">
+        <Link to="/portfolio" className="item">
+          <h2 className="ui header">
+            <div className="content">Portfolio</div>
+          </h2>
+        </Link>
+        <NavLink to="/trade" activeClassName="active item" className="item">
+          <h3 className="ui header">Trade</h3>
+        </NavLink>
+        <NavLink
+          exact
+          to="/research"
+          activeClassName="active item"
+          className="item"
+        >
+          <h3 className="ui header">Research</h3>
+        </NavLink>
+      </div>
+    );
   }
 }
-
 export default Navbar;
