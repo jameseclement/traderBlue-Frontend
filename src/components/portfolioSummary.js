c379d2405ca090b36cff4a15c82df5b773f45814
James Clement <jameseclement@gmail.com>
2019-04-05 16:26:22 -0400

    can show cash

--- src/components/portfolioSummary.js
+++ src/components/portfolioSummary.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 class PortfolioSummary extends Component {
   render() {
+    // let investements = this.props.portfolio.positions.map(p => {
+    //   return p.quantity * p.info.quote.latestPrice;
+    // });
+
     return (
       <div>
-        Cash:
+        Cash:{this.props.portfolio.cash}
         <br />
-        Investments:
+        Investments:{this.props.portfolio}
         <br />
         Total:
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    portfolio: state.portfolio
+  };
+};
 
-export default PortfolioSummary;
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(PortfolioSummary)
+);
