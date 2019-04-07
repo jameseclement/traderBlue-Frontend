8871c40abdf8d19fdc2f2b03949952d6dd637223
James Clement <jameseclement@gmail.com>
2019-04-06 23:17:47 -0400

    swap day range

--- src/components/tradingData.js
+++ src/components/tradingData.js
@@ -8,48 +8,52 @@ class TradingData extends Component {
       <div>Loading</div>
     ) : (
       <table>
-        <tr>
-          <th colspan="2">{`${this.props.stock.quote.companyName} Info`}</th>
-        </tr>
-        <tr>
-          <td>Latest Price</td>
-          <td>${this.props.stock.quote.latestPrice}</td>
-        </tr>
-        <tr>
-          <td>Day Change ($)</td>
-          <td>${this.props.stock.quote.change}</td>
-        </tr>
-        <tr>
-          <td>Day Change (%)</td>
-          <td>{(this.props.stock.quote.changePercent * 100).toFixed(2)}%</td>
-        </tr>
-        <tr>
-          <td>Day Range</td>
-          <td>
-            ${`${this.props.stock.quote.high} - ${this.props.stock.quote.low}`}
-          </td>
-        </tr>
-        <tr>
-          <td>Market Cap </td>
-          <td>
-            ${`${(this.props.stock.quote.marketCap / 1000000000).toFixed(2)}`}B
-          </td>
-        </tr>
-        <tr>
-          <td>P/E Ratio</td>
-          <td>{this.props.stock.quote.peRatio}</td>
-        </tr>
-        <tr>
-          <td>52 Week Range</td>
-          <td>
-            ${this.props.stock.quote.week52Low.toFixed(2)}-
-            {this.props.stock.quote.week52High.toFixed(2)}
-          </td>
-        </tr>
-        <tr>
-          <td>Day Low</td>
-          <td>${this.props.stock.quote.low}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th colSpan="2">{`${this.props.stock.quote.companyName} Info`}</th>
+          </tr>
+          <tr>
+            <td>Latest Price</td>
+            <td>${this.props.stock.quote.latestPrice}</td>
+          </tr>
+          <tr>
+            <td>Day Change ($)</td>
+            <td>${this.props.stock.quote.change}</td>
+          </tr>
+          <tr>
+            <td>Day Change (%)</td>
+            <td>{(this.props.stock.quote.changePercent * 100).toFixed(2)}%</td>
+          </tr>
+          <tr>
+            <td>Day Range</td>
+            <td>
+              $
+              {`${this.props.stock.quote.low} - ${this.props.stock.quote.high}`}
+            </td>
+          </tr>
+          <tr>
+            <td>Market Cap </td>
+            <td>
+              ${`${(this.props.stock.quote.marketCap / 1000000000).toFixed(2)}`}
+              B
+            </td>
+          </tr>
+          <tr>
+            <td>P/E Ratio</td>
+            <td>{this.props.stock.quote.peRatio}</td>
+          </tr>
+          <tr>
+            <td>52 Week Range</td>
+            <td>
+              ${this.props.stock.quote.week52Low.toFixed(2)}-
+              {this.props.stock.quote.week52High.toFixed(2)}
+            </td>
+          </tr>
+          <tr>
+            <td>Day Low</td>
+            <td>${this.props.stock.quote.low}</td>
+          </tr>
+        </tbody>
       </table>
     );
   }
