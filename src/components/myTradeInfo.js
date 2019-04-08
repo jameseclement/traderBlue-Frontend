c4517109b4b04874feccaafb7a7d53d2556482dc
James Clement <jameseclement@gmail.com>
2019-04-08 10:40:55 -0400

    write MyTradeInfo Component showing current position and available cash

---
+++ src/components/myTradeInfo.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+class MyTradeInfo extends Component {
+  render() {
+    return !this.props.portfolio.cash ? (
+      <div>loading</div>
+    ) : (
+      <div>
+        <h3>Current Position: {this.props.position.quantity} Shares</h3>
+
+        <h3>Available Cash: ${this.props.portfolio.cash.toFixed(2)}</h3>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    position: state.position,
+    portfolio: state.portfolio
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(MyTradeInfo)
+);
--- src/containers/tradePage.js
+++ src/containers/tradePage.js
@@ -5,21 +5,26 @@ import SecurityChart from "../components/securityChart";
 import NewsContainer from "../components/newsContainer";
 import TradingData from "../components/tradingData";
 import TradeControls from "../components/tradeControls";
+import MyTradeInfo from "../components/myTradeInfo";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
 class TradePage extends Component {
   render() {
     // let ticker = this.props.match.params.id;
-    return (
+    return !this.props.match.params.id ? (
+      <div>Loading</div>
+    ) : (
       <div>
-        <h1>Trade</h1>
+        <h1>Trade {this.props.match.params.id}</h1>
         <br />
         <br />
         <br />
         <br />
         <TradeControls />
         <TradingData />
+        <MyTradeInfo />
+
         <br />
         <br />
         <br />
@@ -38,4 +43,4 @@ class TradePage extends Component {
 //     ticker: state.stock.ticker
 //   };
 // };
-export default TradePage;
+export default withRouter(TradePage);
