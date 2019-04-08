fa2bbe58142835e8d0b99ab6c47e2a8f2eff4ad5
James Clement <jameseclement@gmail.com>
2019-04-08 18:57:48 -0400

    watchlist items can link to correct trade page

--- src/components/watchlistItem.js
+++ src/components/watchlistItem.js
@@ -1,16 +1,44 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { fetchingPosition } from "../redux/actions";
+import { fetchingStock } from "../redux/actions";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 class WatchlistItem extends Component {
   render() {
+    const fetchInfo = ticker => {
+      this.props.fetchingPosition(ticker);
+      this.props.fetchingStock(ticker);
+    };
     return (
       <li>
-        <Link to={`/trade/${this.props.stock.ticker}`}>
+        <Link
+          onClick={() => {
+            this.fetchInfo(this.props.stock.ticker);
+          }}
+          to={`/trade/${this.props.stock.ticker}`}
+        >
           {this.props.stock.ticker}
         </Link>
       </li>
     );
   }
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchingPosition: () => {
+      dispatch(fetchingPosition());
+    },
+    fetchingStock: () => {
+      dispatch(fetchingStock());
+    }
+  };
+};
 
-export default WatchlistItem;
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(WatchlistItem)
+);
