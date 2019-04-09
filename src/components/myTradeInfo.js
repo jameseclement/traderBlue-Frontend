6cdbc53809c4f143c1688e87c9f65b3323ac4e6c
James Clement <jameseclement@gmail.com>
2019-04-09 17:55:50 -0400

    ALL numbers run through numerators

--- src/components/myTradeInfo.js
+++ src/components/myTradeInfo.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-
+var numeral = require("numeral");
 class MyTradeInfo extends Component {
   render() {
     return !this.props.portfolio.cash ? (
@@ -10,9 +10,14 @@ class MyTradeInfo extends Component {
       <div>
         <h2>
           Current Position:{" "}
-          {this.props.position ? this.props.position.quantity : "0"} Shares
+          {this.props.position
+            ? numeral(this.props.position.quantity).format("0,0")
+            : "0"}{" "}
+          Shares
         </h2>
-        <h2>Available Cash: ${this.props.portfolio.cash.toFixed(2)}</h2>
+        <h2>
+          Available Cash: {numeral(this.props.portfolio.cash).format("$0,0.00")}
+        </h2>
       </div>
     );
   }
