ac808214c40a2a5fdf1069c12810a84d310a3837
James Clement <jameseclement@gmail.com>
2019-04-09 16:44:22 -0400

    Portfolio page able to display empty portfolio

--- src/components/portfolioSummary.js
+++ src/components/portfolioSummary.js
@@ -6,6 +6,37 @@ class PortfolioSummary extends Component {
   render() {
     if (!this.props.portfolio.positions) {
       return <div>Loading Positions </div>;
+    } else if (this.props.portfolio.positions.length == 0) {
+      let invested = 0;
+      return (
+        <div>
+          <table className="ui striped table">
+            <thead>
+              <tr className="">
+                <th colSpan="2" className="">
+                  Portfolio Summary
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="">
+                <td className="">Cash</td>
+                <td className="">${this.props.portfolio.cash.toFixed(2)}</td>
+              </tr>
+              <tr className="">
+                <td className="">Invested</td>
+                <td className="">${invested.toFixed(2)}</td>
+              </tr>
+              <tr className="">
+                <td className="">Total</td>
+                <td className="">
+                  ${(this.props.portfolio.cash + invested).toFixed(2)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
     } else {
       let investments = this.props.portfolio.positions.map(p => {
         return p.quantity * p.info.quote.latestPrice;
@@ -13,6 +44,7 @@ class PortfolioSummary extends Component {
       let invested = investments.reduce((x, y) => {
         return x + y;
       });
+
       return (
         <div>
           <table className="ui striped table">
@@ -45,7 +77,6 @@ class PortfolioSummary extends Component {
     }
   }
 }
-
 const mapStateToProps = state => {
   return {
     portfolio: state.portfolio
