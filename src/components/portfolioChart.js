2eadb5fff3ec7ed93fcbaa3b84e3c1591e803e7e
James Clement <jameseclement@gmail.com>
2019-04-14 16:25:37 -0400

    first attempt at portfolio pie chart, not implemented though

--- src/components/portfolioChart.js
+++ src/components/portfolioChart.js
@@ -2,35 +2,48 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 var PieChart = require("react-chartjs").Pie;
-var data = [
-  {
-    value: 300,
-    color: "#F7464A",
-    highlight: "#FF5A5E",
-    label: "Red"
-  },
-  {
-    value: 50,
-    color: "#46BFBD",
-    highlight: "#5AD3D1",
-    label: "Green"
-  },
-  {
-    value: 100,
-    color: "#FDB45C",
-    highlight: "#FFC870",
-    label: "Yellow"
-  },
-  {
-    value: 4,
-    color: "Green",
-    highlight: "#FFC870",
-    label: "Green"
-  }
-];
+
 class PortfolioChart extends Component {
   render() {
-    return <PieChart data={data} />;
+    if (!!this.props.portfolio.positions) {
+      let values = this.props.portfolio.positions.map(p => {
+        return p.info.quote.latestPrice * p.quantity;
+      });
+
+      let data = [
+        {
+          value: values[0],
+          color: "#F7464A",
+          highlight: "#FF5A5E",
+          label: "Red"
+        },
+        {
+          value: values[1],
+          color: "#46BFBD",
+          highlight: "#5AD3D1",
+          label: "Green"
+        },
+        {
+          value: values[2],
+          color: "#FDB45C",
+          highlight: "#FFC870",
+          label: "Yellow"
+        },
+        {
+          value: values[3],
+          color: "Green",
+          highlight: "#FFC870",
+          label: "Green"
+        }
+      ];
+      return !!this.props.portfolio ? (
+        <PieChart data={data} />
+      ) : (
+        <div>Loading</div>
+      );
+    } else {
+      return <div>Loading</div>;
+    }
   }
 }
 
--- src/containers/portfolioPage.js
+++ src/containers/portfolioPage.js
@@ -38,6 +38,7 @@ class PortfolioPage extends Component {
           <PortfolioSummary />
         </div>
         <div className="three wide column">
+          <PortfolioChart />
           <Watchlist />
         </div>
       </div>
