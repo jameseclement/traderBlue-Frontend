a0ab397d26b05c49fddc69922d39f73e7b2020be
James Clement <jameseclement@gmail.com>
2019-04-15 18:26:11 -0400

    Portfoio summary table uses semantic ui components

--- src/components/portfolioSummary.js
+++ src/components/portfolioSummary.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { Table } from "semantic-ui-react";
 var numeral = require("numeral");
+
 class PortfolioSummary extends Component {
   render() {
     if (!this.props.portfolio.positions) {
@@ -13,37 +15,36 @@ class PortfolioSummary extends Component {
     } else if (this.props.portfolio.positions.length === 0) {
       let invested = 0;
       return (
-        <div>
-          <table className="ui striped table">
-            <thead>
-              <tr className="">
-                <th colSpan="2" className="">
-                  Portfolio Summary
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="">
-                <td className="">Cash</td>
-                <td className="">
-                  {numeral(this.props.portfolio.cash).format("$0,0.00")}
-                </td>
-              </tr>
-              <tr className="">
-                <td className="">Invested</td>
-                <td className="">{numeral(invested).format("$0,0.00")}</td>
-              </tr>
-              <tr className="">
-                <td className="">Total</td>
-                <td className="">
-                  {numeral(this.props.portfolio.cash + invested).format(
-                    "$0,0.00"
-                  )}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <Table striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell colSpan="2">Portfolio Summary</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row className="">
+              <Table.Cell className="">Cash</Table.Cell>
+              <Table.Cell className="">
+                {numeral(this.props.portfolio.cash).format("$0,0.00")}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row className="">
+              <Table.Cell className="">Invested</Table.Cell>
+              <Table.Cell className="">
+                {numeral(invested).format("$0,0.00")}
+              </Table.Cell>
+              <Table.Cell />
+            </Table.Row>
+            <Table.Row className="">
+              <Table.Cell className="">Total</Table.Cell>
+              <Table.Cell className="">
+                {numeral(this.props.portfolio.cash + invested).format(
+                  "$0,0.00"
+                )}
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       );
     } else {
       let investments = this.props.portfolio.positions.map(p => {
